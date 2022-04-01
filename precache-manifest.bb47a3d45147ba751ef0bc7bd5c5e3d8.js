fd6f2d0e8e436551b4c858fb2416a29b32a774e0
marcinstenka <mastenka@wp.pl>
2022-04-01 15:30:25 +0200

    Updates

---
+++ precache-manifest.bb47a3d45147ba751ef0bc7bd5c5e3d8.js
@@ -0,0 +1,502 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425d0bb451093275e198eae6f00965d8",
+    "url": "/becker/index.html"
+  },
+  {
+    "revision": "e9656d026724054f6833",
+    "url": "/becker/static/css/main.2fa3de6c.chunk.css"
+  },
+  {
+    "revision": "75c1fb9d5e2712497fda",
+    "url": "/becker/static/js/2.0784a281.chunk.js"
+  },
+  {
+    "revision": "8c4b3bd464f69dcff9dd9560811d82a4",
+    "url": "/becker/static/js/2.0784a281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9656d026724054f6833",
+    "url": "/becker/static/js/main.9d75624c.chunk.js"
+  },
+  {
+    "revision": "9cafed33b405efb91379",
+    "url": "/becker/static/js/runtime-main.7ab9c35d.js"
+  },
+  {
+    "revision": "0328e4ecaafced458f0efb8947abc809",
+    "url": "/becker/static/media/1.0328e4ec.jpg"
+  },
+  {
+    "revision": "0ac80ffc12e25f438e200e5c1d4debb9",
+    "url": "/becker/static/media/1.0ac80ffc.jpg"
+  },
+  {
+    "revision": "0d3dec3bac03bbd9a867dfbc57c6871d",
+    "url": "/becker/static/media/1.0d3dec3b.JPG"
+  },
+  {
+    "revision": "3a90101bd47a0e2d1f72d6cbf3eb5272",
+    "url": "/becker/static/media/1.3a90101b.JPG"
+  },
+  {
+    "revision": "4bf9a75ebf43299a573b4572eff40cbc",
+    "url": "/becker/static/media/1.4bf9a75e.JPG"
+  },
+  {
+    "revision": "6385eadb70d8de2c93a906f85e422e82",
+    "url": "/becker/static/media/1.6385eadb.JPG"
+  },
+  {
+    "revision": "63b362a26810c6de3a54d78078723071",
+    "url": "/becker/static/media/1.63b362a2.JPG"
+  },
+  {
+    "revision": "6dde700e44d809ab28af2f11058b8fb2",
+    "url": "/becker/static/media/1.6dde700e.JPG"
+  },
+  {
+    "revision": "6df4e447ceb45b9c58bd55b4cb495434",
+    "url": "/becker/static/media/1.6df4e447.JPG"
+  },
+  {
+    "revision": "7750107ea0fd3d349f545e04272524a9",
+    "url": "/becker/static/media/1.7750107e.JPG"
+  },
+  {
+    "revision": "874492230f51b032b483cad961d3ce8f",
+    "url": "/becker/static/media/1.87449223.JPG"
+  },
+  {
+    "revision": "894ab640f306926bc185d0838b9f313e",
+    "url": "/becker/static/media/1.894ab640.jpg"
+  },
+  {
+    "revision": "aeb76194bc3d8bc9171e3a55a3e8d350",
+    "url": "/becker/static/media/1.aeb76194.JPG"
+  },
+  {
+    "revision": "c1a299f6c6e3d5f4b8fc7ea68bec4163",
+    "url": "/becker/static/media/1.c1a299f6.jpg"
+  },
+  {
+    "revision": "c4f242708446c1b2224563141725e3bb",
+    "url": "/becker/static/media/1.c4f24270.JPG"
+  },
+  {
+    "revision": "c7526bf8f89c2b7df82d7b69634b8eca",
+    "url": "/becker/static/media/1.c7526bf8.jpg"
+  },
+  {
+    "revision": "cdcd188b476fe46dd69d1c9b64997a2a",
+    "url": "/becker/static/media/1.cdcd188b.jpg"
+  },
+  {
+    "revision": "e2274a0977ed2fb6c22093f8e796b530",
+    "url": "/becker/static/media/1.e2274a09.JPG"
+  },
+  {
+    "revision": "f1724b782d64e78f5ba3031250fc5399",
+    "url": "/becker/static/media/1.f1724b78.JPG"
+  },
+  {
+    "revision": "fbd2fb6f84b7397d1d2cfe43018b0379",
+    "url": "/becker/static/media/1.fbd2fb6f.JPG"
+  },
+  {
+    "revision": "030b8f907d5cbe38b721c3a0656f6c79",
+    "url": "/becker/static/media/2.030b8f90.JPG"
+  },
+  {
+    "revision": "0926831fe965073a31b24ef7d4b0e836",
+    "url": "/becker/static/media/2.0926831f.JPG"
+  },
+  {
+    "revision": "16bc4e8ec061e47f3007d5de634b3aae",
+    "url": "/becker/static/media/2.16bc4e8e.JPG"
+  },
+  {
+    "revision": "172067db54b5f689cca4d1b60f6b7607",
+    "url": "/becker/static/media/2.172067db.jpg"
+  },
+  {
+    "revision": "18f607f6e7061af30da70b169bda938e",
+    "url": "/becker/static/media/2.18f607f6.JPG"
+  },
+  {
+    "revision": "1d9d7ac151179b4c79ecb7f8f0f44ed8",
+    "url": "/becker/static/media/2.1d9d7ac1.JPG"
+  },
+  {
+    "revision": "2be0e613acd5ef6724cb74c17e850462",
+    "url": "/becker/static/media/2.2be0e613.JPG"
+  },
+  {
+    "revision": "33861d30d5465f2e9ea7d7d897a6b223",
+    "url": "/becker/static/media/2.33861d30.JPG"
+  },
+  {
+    "revision": "33bee423db2efa7c2eb6a5c73b0faf2f",
+    "url": "/becker/static/media/2.33bee423.JPG"
+  },
+  {
+    "revision": "53347db2da94c78431ba6ffa327324d7",
+    "url": "/becker/static/media/2.53347db2.JPG"
+  },
+  {
+    "revision": "56c106f9b5405007e53cd3ba46ac1584",
+    "url": "/becker/static/media/2.56c106f9.JPG"
+  },
+  {
+    "revision": "5eeb8bd29a86f074aa509a8cd69d2b6a",
+    "url": "/becker/static/media/2.5eeb8bd2.JPG"
+  },
+  {
+    "revision": "7306701cd453278dc00a4284b8e212e8",
+    "url": "/becker/static/media/2.7306701c.jpg"
+  },
+  {
+    "revision": "797b43b9c340f414ef19a069aa3bb47d",
+    "url": "/becker/static/media/2.797b43b9.jpg"
+  },
+  {
+    "revision": "833497265a547a18a9d1459d9e97b7e8",
+    "url": "/becker/static/media/2.83349726.jpg"
+  },
+  {
+    "revision": "9c9b0866eaa7f2dd10a6e9e4d014708e",
+    "url": "/becker/static/media/2.9c9b0866.JPG"
+  },
+  {
+    "revision": "e3c5b3c1ff3eef308b82f717e67e5c08",
+    "url": "/becker/static/media/2.e3c5b3c1.JPG"
+  },
+  {
+    "revision": "f658c53c75aa02dda0f0c3002af36669",
+    "url": "/becker/static/media/2.f658c53c.JPG"
+  },
+  {
+    "revision": "f8dd7e5b8baa98e2f4acea0cdf6445d1",
+    "url": "/becker/static/media/2.f8dd7e5b.jpg"
+  },
+  {
+    "revision": "faee968aded6e47582f598f0025ca919",
+    "url": "/becker/static/media/2.faee968a.jpg"
+  },
+  {
+    "revision": "09a02b2c0c6101b73815f9f352f3ccab",
+    "url": "/becker/static/media/3.09a02b2c.JPG"
+  },
+  {
+    "revision": "1b2133b1f1a7ad26de0174eda48419bb",
+    "url": "/becker/static/media/3.1b2133b1.jpg"
+  },
+  {
+    "revision": "310c108f7f6b3015e3bba83c54e65798",
+    "url": "/becker/static/media/3.310c108f.JPG"
+  },
+  {
+    "revision": "35aa560dbc90a10b71edcde087334644",
+    "url": "/becker/static/media/3.35aa560d.jpg"
+  },
+  {
+    "revision": "374523e712c3fdf1fbf4231720b10015",
+    "url": "/becker/static/media/3.374523e7.JPG"
+  },
+  {
+    "revision": "39784e2bf4f725f8b7690047b0304f5f",
+    "url": "/becker/static/media/3.39784e2b.JPG"
+  },
+  {
+    "revision": "421a04c0fe9416fa312574e4b4a1affd",
+    "url": "/becker/static/media/3.421a04c0.JPG"
+  },
+  {
+    "revision": "5521581fca70861334bd5731de972c78",
+    "url": "/becker/static/media/3.5521581f.JPG"
+  },
+  {
+    "revision": "59c12253fc8db79e59e008179ab5de0e",
+    "url": "/becker/static/media/3.59c12253.jpg"
+  },
+  {
+    "revision": "8015235af727fa20098fae34b49bc032",
+    "url": "/becker/static/media/3.8015235a.JPG"
+  },
+  {
+    "revision": "97d1b510faf6c25b9b266ba325ea8a7b",
+    "url": "/becker/static/media/3.97d1b510.jpg"
+  },
+  {
+    "revision": "a0904e92f344805a0d017796c8c1fb8f",
+    "url": "/becker/static/media/3.a0904e92.jpg"
+  },
+  {
+    "revision": "cb1863754134f09db272ef50a3b5bb07",
+    "url": "/becker/static/media/3.cb186375.JPG"
+  },
+  {
+    "revision": "da7f67354382274d4572f5bd9a835987",
+    "url": "/becker/static/media/3.da7f6735.jpg"
+  },
+  {
+    "revision": "dac7fe1b82adc4badde266c0e4d22fd2",
+    "url": "/becker/static/media/3.dac7fe1b.JPG"
+  },
+  {
+    "revision": "e073aad70c7f5852e606cb51261e0191",
+    "url": "/becker/static/media/3.e073aad7.JPG"
+  },
+  {
+    "revision": "ea37ee11c25eb5ea10390400e1280403",
+    "url": "/becker/static/media/3.ea37ee11.JPG"
+  },
+  {
+    "revision": "efaa291f5012c2cc6954b6616e347df5",
+    "url": "/becker/static/media/3.efaa291f.JPG"
+  },
+  {
+    "revision": "f33bab8d080cfc0cf26f2ae5b988e072",
+    "url": "/becker/static/media/3.f33bab8d.JPG"
+  },
+  {
+    "revision": "f66e61a4699c10cc36e271d7311d6378",
+    "url": "/becker/static/media/3.f66e61a4.JPG"
+  },
+  {
+    "revision": "0c30c35151e205484d67534e9a013f74",
+    "url": "/becker/static/media/4.0c30c351.JPG"
+  },
+  {
+    "revision": "29860edd0e13c6c0f906e4de23e39ef7",
+    "url": "/becker/static/media/4.29860edd.JPG"
+  },
+  {
+    "revision": "385d63e467a0c3db6d1030466f94d547",
+    "url": "/becker/static/media/4.385d63e4.jpg"
+  },
+  {
+    "revision": "56795efa2b06288fb5175e0550a6a748",
+    "url": "/becker/static/media/4.56795efa.JPG"
+  },
+  {
+    "revision": "5e28a0fc9e4e6e34156a119fc06e344d",
+    "url": "/becker/static/media/4.5e28a0fc.JPG"
+  },
+  {
+    "revision": "6edbc771cf51c17af9f8530edb398383",
+    "url": "/becker/static/media/4.6edbc771.jpg"
+  },
+  {
+    "revision": "7089b80f58eb5ed204a28fc330d05b24",
+    "url": "/becker/static/media/4.7089b80f.JPG"
+  },
+  {
+    "revision": "8748a8480a2650f1ac32d31c9998b53c",
+    "url": "/becker/static/media/4.8748a848.jpg"
+  },
+  {
+    "revision": "b4d9989c816a503721df063c3d3190a6",
+    "url": "/becker/static/media/4.b4d9989c.jpg"
+  },
+  {
+    "revision": "c229d74e1916693cdbccde5f2560c7ec",
+    "url": "/becker/static/media/4.c229d74e.JPG"
+  },
+  {
+    "revision": "e04d71418b861d53865ae266d82179bf",
+    "url": "/becker/static/media/4.e04d7141.JPG"
+  },
+  {
+    "revision": "ecd4a566cb26ea0a841127eb52d8c55d",
+    "url": "/becker/static/media/4.ecd4a566.JPG"
+  },
+  {
+    "revision": "23d8eaacd765bf97f2ce95b7e53cadee",
+    "url": "/becker/static/media/5.23d8eaac.JPG"
+  },
+  {
+    "revision": "64b2cc66bacb7862b17d97d0a172b054",
+    "url": "/becker/static/media/5.64b2cc66.JPG"
+  },
+  {
+    "revision": "6ab7b28de11b72485ba54c2bc4554e07",
+    "url": "/becker/static/media/5.6ab7b28d.JPG"
+  },
+  {
+    "revision": "d5bd3fbe2ce9c3a86b02718208d22ad7",
+    "url": "/becker/static/media/5.d5bd3fbe.JPG"
+  },
+  {
+    "revision": "db97dc6d8d5c05a599baeebbe60a3dfb",
+    "url": "/becker/static/media/5.db97dc6d.jpg"
+  },
+  {
+    "revision": "f9d75a75d89885fa364c8959a039f322",
+    "url": "/becker/static/media/5.f9d75a75.jpg"
+  },
+  {
+    "revision": "5b2b549bba3ca768875c7cf0093f6f30",
+    "url": "/becker/static/media/6.5b2b549b.jpg"
+  },
+  {
+    "revision": "d7ef5b873caec16b899e8008196ecaf5",
+    "url": "/becker/static/media/6.d7ef5b87.jpg"
+  },
+  {
+    "revision": "8694871fda4d547c26890479119e815b",
+    "url": "/becker/static/media/7.8694871f.jpg"
+  },
+  {
+    "revision": "9f4876183d44f1aa193c80f32a124a03",
+    "url": "/becker/static/media/7.9f487618.jpg"
+  },
+  {
+    "revision": "0fa27061305a9587c1975a046791f9f6",
+    "url": "/becker/static/media/8.0fa27061.jpg"
+  },
+  {
+    "revision": "87650238b6bb5b4b8e0d95af0cf76c05",
+    "url": "/becker/static/media/8.87650238.jpg"
+  },
+  {
+    "revision": "ed8ac77c47be8f645ff37bd91a4c32af",
+    "url": "/becker/static/media/Regulamin-obozu-naukowego.ed8ac77c.pdf"
+  },
+  {
+    "revision": "73dd49bb02b634be8b24cff218be68e5",
+    "url": "/becker/static/media/as1.73dd49bb.jpg"
+  },
+  {
+    "revision": "9e8d37f0a03ecad268fe2f95c14dac91",
+    "url": "/becker/static/media/as2.9e8d37f0.jpg"
+  },
+  {
+    "revision": "b4af4f07609132ecd151ddf28d0a4dd9",
+    "url": "/becker/static/media/as3.b4af4f07.jpg"
+  },
+  {
+    "revision": "9e65e651b5782c25b5ca677d643c74e1",
+    "url": "/becker/static/media/bookBig.9e65e651.jpg"
+  },
+  {
+    "revision": "e4388e5f33f73ece2df8889a8edf5033",
+    "url": "/becker/static/media/bookSmall.e4388e5f.jpg"
+  },
+  {
+    "revision": "577e7d3036be0083d8854f369a8df3a8",
+    "url": "/becker/static/media/bp1.577e7d30.jpg"
+  },
+  {
+    "revision": "4d58b3824acd2f687e26c1197d1791ed",
+    "url": "/becker/static/media/bp2.4d58b382.jpg"
+  },
+  {
+    "revision": "c3477d16a93803c18356ba66158f1da9",
+    "url": "/becker/static/media/bp3.c3477d16.jpg"
+  },
+  {
+    "revision": "2f2bf7fec36560efb73beee58f3480b9",
+    "url": "/becker/static/media/cz1.2f2bf7fe.jpg"
+  },
+  {
+    "revision": "15ad70e84f0a956fa569d0a972153f77",
+    "url": "/becker/static/media/cz2.15ad70e8.jpg"
+  },
+  {
+    "revision": "29c0b1609c80af7d55d43ab0ad52772c",
+    "url": "/becker/static/media/cz3.29c0b160.jpg"
+  },
+  {
+    "revision": "bda8ee1ddc93de0daa1639e71c69b11e",
+    "url": "/becker/static/media/facebookIcon.bda8ee1d.svg"
+  },
+  {
+    "revision": "937602ca12cfaccd1167b8e92717c4d3",
+    "url": "/becker/static/media/heroDesktop.937602ca.jpg"
+  },
+  {
+    "revision": "0c1d78c294cce4bfb3568a869ac151e6",
+    "url": "/becker/static/media/heroMobile.0c1d78c2.jpg"
+  },
+  {
+    "revision": "96451149e65ca4d48a950ddbebc00e9d",
+    "url": "/becker/static/media/heroSuperDesktop.96451149.jpg"
+  },
+  {
+    "revision": "30fa0886f8b6840a53f5deb91f9722f5",
+    "url": "/becker/static/media/ig.30fa0886.png"
+  },
+  {
+    "revision": "7e51fefcbf60ac3a39e30b6dbfe05316",
+    "url": "/becker/static/media/instagramIcon.7e51fefc.svg"
+  },
+  {
+    "revision": "53dfe7e18a0e20fb4519762af8140a24",
+    "url": "/becker/static/media/ir1.53dfe7e1.jpg"
+  },
+  {
+    "revision": "753a2c93112bca00bef38bc257ecfd27",
+    "url": "/becker/static/media/ir2.753a2c93.jpg"
+  },
+  {
+    "revision": "642cd1f00665a28e6fe1cf28253bbb1a",
+    "url": "/becker/static/media/ir3.642cd1f0.jpg"
+  },
+  {
+    "revision": "7ecd9cd134e0b7d02cf229937091aff2",
+    "url": "/becker/static/media/karta-uczestnika.7ecd9cd1.pdf"
+  },
+  {
+    "revision": "55c07abb592e2df5d9c578c7eba37da0",
+    "url": "/becker/static/media/logo.55c07abb.png"
+  },
+  {
+    "revision": "d2b5d27badb1042bc1b0de95a1cd936c",
+    "url": "/becker/static/media/mailpostBig.d2b5d27b.jpg"
+  },
+  {
+    "revision": "6dc89a1949e8086370c38dc575462c5f",
+    "url": "/becker/static/media/mailpostSmall.6dc89a19.jpg"
+  },
+  {
+    "revision": "af7fba213719ca20e2e58cf3d76e61dc",
+    "url": "/becker/static/media/mainHeroMobile.af7fba21.jpg"
+  },
+  {
+    "revision": "71dd9566b2e3f83f6c68dc12cbf002c7",
+    "url": "/becker/static/media/nz1.71dd9566.jpg"
+  },
+  {
+    "revision": "d11ec9b3ef0a8aa84fb5485e22ff0e78",
+    "url": "/becker/static/media/nz2.d11ec9b3.jpg"
+  },
+  {
+    "revision": "f6de20b1a53cab6eaf4ec05ba7398aa2",
+    "url": "/becker/static/media/nz3.f6de20b1.jpg"
+  },
+  {
+    "revision": "65523448d9a2b9a22f8acc62a516825f",
+    "url": "/becker/static/media/opinia-nauczyciela-matematyki-fizyki.65523448.pdf"
+  },
+  {
+    "revision": "af99c70f4912b32a16c51a6fe7367e3d",
+    "url": "/becker/static/media/rozklad-dnia.af99c70f.pdf"
+  },
+  {
+    "revision": "052854b5a52e85e1fd61394ffe113ebf",
+    "url": "/becker/static/media/umowa-oboz.052854b5.pdf"
+  },
+  {
+    "revision": "3e58136ed5998e1af2ac008db2ed5c3a",
+    "url": "/becker/static/media/wm1.3e58136e.jpg"
+  },
+  {
+    "revision": "f8c962db580c0b503e9f88c6a0b51032",
+    "url": "/becker/static/media/wm2.f8c962db.jpg"
+  },
+  {
+    "revision": "96f40646189bfa50a4b3ac1c2db2140b",
+    "url": "/becker/static/media/wm3.96f40646.jpg"
+  }
+]);
