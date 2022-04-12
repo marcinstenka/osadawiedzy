fb595e5da443674af0c4b38199b9180993e96abf
marcinstenka <mastenka@wp.pl>
2022-04-12 20:41:31 +0200

    0.5.9 - safari test 4

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,10 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 import TheBoldFont from './assets/fonts/theboldfont.ttf';
+import test from './assets/fonts/roboto-condensed.bold.ttf';
 const GlobalStyle = createGlobalStyle`
         
         @font-face{
             font-family: 'the bold font';
             src: url(${TheBoldFont}) format('truetype');
+        };
+        @font-face{
+            font-family: 'test';
+            src: url(${test}) format('truetype');
         };
     `;
 export default GlobalStyle;
