2402c531018d42627dc2ecc0ae0ff4240c1c3f3f
Marcin Stenka <mastenka@wp.pl>
2022-09-12 16:53:19 +0200

    0.8 - email.js added

--- src/components/ScienceMail.js
+++ src/components/ScienceMail.js
@@ -1,39 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import ScienceArrow from '../assets/img/scienceArrow.svg';
 
 const ScienceMail = color => {
-  const [buttonColor, setButtonColor] = useState(color);
-  return (
-    <div className="science-register">
-      <h3>ZAPISY</h3>
-      <p className="science-text">
-        W celu zapisania się na kurs zapraszamy do kontaktu telefonicznego,
-        wysłania wiadomości na adres zaranskanatalia@gmail.com lub skorzystania
-        z poniższego formularza.
-      </p>
-      <form className="register-form">
-        <label htmlFor="register-name">Imię i nazwisko:</label>
-        <input type="text" name="register-name" required />
-        <label htmlFor="register-mail">Adres e-mail:</label>
-        <input type="text" name="register-email" required />
-        <label htmlFor="register-phone">Numer telefonu:</label>
-        <input type="text" name="register-phone" required />
-        <label htmlFor="register-type">Zajęcia:</label>
-        <div className="register-type"></div>
-        <select name="register-type" required>
-          <option value="0">wybierz z listy</option>
-          <option value="1">Fizyka</option>
-          <option value="2">Matematyka</option>
-          <option value="3">Programowanie</option>
-        </select>
-        <label htmlFor="register-message">Twoja wiadomość</label>
-        <input type="text" name="register-message" />
-        <button style={{ background: buttonColor.color }}>
-          wyślij <img src={ScienceArrow} alt="Strzałka zobacz więcej." />
-        </button>
-      </form>
-    </div>
-  );
+	const [buttonColor, setButtonColor] = useState(color);
+	const [status, setStatus] = useState('');
+	const form = useRef();
+	useEffect(() => {
+		const registerStatus = document.querySelector('#register-status');
+		const inputs = document.querySelectorAll('input');
+		if (registerStatus) {
+			registerStatus.innerHTML = status;
+			inputs.forEach(input => {
+				input.value = '';
+			});
+		}
+	});
+	const sendEmail = e => {
+		e.preventDefault();
+		emailjs
+			.sendForm(
+				'osadatest',
+				'template_w9a8tbs',
+				form.current,
+				'zCfJ_UFvXZ_U3x64A'
+			)
+			.then(
+				result => {
+					setStatus('Dziękujemy za wiadomość, wkrótce odpiszemy! :)');
+				},
+				error => {
+					alert('Coś poszło nie tak. Spróbuj ponownie za chwilę.');
+				}
+			);
+	};
+	return (
+		<div className='science-register'>
+			<h3>ZAPISY</h3>
+			<p className='science-text'>
+				W celu zapisania się na kurs zapraszamy do kontaktu telefonicznego,
+				wysłania wiadomości na adres zaranskanatalia@gmail.com lub skorzystania
+				z poniższego formularza.
+			</p>
+			<form ref={form} className='register-form' onSubmit={sendEmail}>
+				<label htmlFor='register-name'>Imię i nazwisko:</label>
+				<input type='text' name='register-name' required />
+				<label htmlFor='register-mail'>Adres e-mail:</label>
+				<input type='email' name='register-email' required />
+				<label htmlFor='register-phone'>Numer telefonu:</label>
+				<input type='text' name='register-phone' required />
+				<label htmlFor='register-type'>Zajęcia:</label>
+				<select name='register-type' required>
+					<option value='Fizyka'>Fizyka</option>
+					<option value='Matematyka'>Matematyka</option>
+					<option value='Programowanie'>Programowanie</option>
+				</select>
+				<label htmlFor='register-message'>Twoja wiadomość:</label>
+				<input type='text' name='register-message' />
+				<div className='register-button'>
+					<button style={{ background: buttonColor.color }}>
+						wyślij <img src={ScienceArrow} alt='Strzałka zobacz więcej.' />
+					</button>
+					<div
+						className='register-status'
+						id='register-status'
+						style={{ color: buttonColor.color }}
+					></div>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default ScienceMail;
---
+++ src/email.js
@@ -0,0 +1,4 @@
+export default {
+	USER_ID: `zCfJ_UFvXZ_U3x64A`, //userID
+	TEMPLATE_ID: `template_w9a8tbs`, //templateID
+};
